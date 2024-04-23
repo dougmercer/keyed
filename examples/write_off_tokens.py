411def88ab96e28db60b6d71fc1244d3723602d7
Doug Mercer <dougmerceryt@gmail.com>
2024-04-22 22:33:04 -0400

    Add example for writing off.

--- examples/write_off_tokens.py
+++ examples/write_off_tokens.py
@@ -10,13 +10,13 @@ tokens = lex(content, PythonLexer())
 json_str = format(tokens, manic_pygments.ManicFormatter(style="base16-nord"))
 styled_tokens = manic_pygments.StyledTokens.validate_json(json_str)
 
-scene = Scene(scene_name="write_on_tokens", num_frames=24, width=1920, height=1080)
+scene = Scene(scene_name="write_off_tokens", num_frames=24, width=1920, height=1080)
 code = Code(scene.ctx, styled_tokens, font_size=48)
 
 scene.add(code)
 
 code.tokens[:].write_on(
-    "alpha", lagged_animation=lag_animation(), delay=1, duration=1, start_frame=0
+    "alpha", lagged_animation=lag_animation(start_value=1, end_value=0), delay=1, duration=1, start_frame=0
 )
 
 scene.draw()
