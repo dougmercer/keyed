475ac0b2f7ae8960f4ac340324ee90606e8cf4b8
Doug Mercer <dougmerceryt@gmail.com>
2024-04-24 21:08:13 -0400

    Import previewer

--- src/manic/__init__.py
+++ src/manic/__init__.py
@@ -1,6 +1,7 @@
 from . import color  # noqa
 from . import easing  # noqa
 from . import highlight  # noqa
+from . import previewer  # noqa
 from .animation import *  # noqa
 from .highlight import *  # noqa
 from .scene import *  # noqa
