a0482635e2c8d238d698aaa65c8c8cd3707ba48b
Doug Mercer <dougmerceryt@gmail.com>
2024-04-25 23:31:29 -0400

    loop and pingpong examples

---
+++ examples/pingpong.py
@@ -0,0 +1,27 @@
+from manic import Animation, AnimationType, Code, PingPong, Scene, easing, tokenize
+
+styled_tokens = tokenize(r"import this")
+
+scene = Scene(scene_name="ping_pong", num_frames=90, width=800, height=600)
+code = Code(scene.ctx, styled_tokens, font_size=48)
+
+scene.add(code.lines[0])
+
+a = code.lines[0:1]
+
+a.animate(
+    "y",
+    PingPong(
+        n=3,
+        animation=Animation(
+            start_frame=0,
+            end_frame=12,
+            start_value=50,
+            end_value=150,
+            easing=easing.CubicEaseInOut,
+            animation_type=AnimationType.ABSOLUTE,
+        ),
+    ),
+)
+
+scene.preview()
