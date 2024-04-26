9c5fc3f352ca13f4b54d25824af93bb8cf1d5b32
Doug Mercer <dougmerceryt@gmail.com>
2024-04-26 18:04:51 -0400

    Add example to make sure we can draw selections in scene.

---
+++ examples/draw_selection.py
@@ -0,0 +1,12 @@
+from manic import Code, Scene, tokenize
+
+with open("examples/example.py", "r") as f:
+    content = f.read()
+styled_tokens = tokenize(content)
+
+scene = Scene(scene_name="draw_selection", num_frames=24, width=1920, height=1080)
+code = Code(scene.ctx, styled_tokens, font_size=48)
+
+scene.add(code.tokens[:10])
+
+scene.preview()
