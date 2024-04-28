802e21e3f699a1cbf913298a1a7fea460133e51f
Doug Mercer <dougmerceryt@gmail.com>
2024-04-27 20:24:53 -0400

    Missing pillow

--- src/keyed/previewer.py
+++ src/keyed/previewer.py
@@ -8,7 +8,7 @@ from typing import TYPE_CHECKING
 from PIL import Image, ImageTk
 
 if TYPE_CHECKING:
-    from pygments import Scene
+    from keyed import Scene
 
 
 def create_animation_window(scene: "Scene") -> None:
