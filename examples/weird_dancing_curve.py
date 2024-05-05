324b30895818c03ac99e04a1f247653a251426b5
Doug Mercer <dougmerceryt@gmail.com>
2024-05-05 00:01:24 -0400

    Make a weird example of moving a curve's points individually

---
+++ examples/weird_dancing_curve.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+from keyed import Animation, Circle, Code, Scene, Selection, Trace, easing, tokenize, PingPong, AnimationType
+
+with open("examples/example.py", "r") as f:
+    content = f.read()
+styled_tokens = tokenize(content)
+
+scene = Scene(scene_name="trace", num_frames=24, width=1920, height=1080)
+code = Code(scene.ctx, styled_tokens, font_size=48, alpha=1, x=100, y=100)
+
+s = Selection([code.chars[0], code.chars[10], code.chars[11], code.chars[30], code.chars[39]])
+
+scene.add(*s)
+scene.add(code)
+
+points = np.array([np.array(c.geom().centroid.coords).flatten() for c in s])
+trace = Trace.from_points(scene.ctx, points, alpha=0.5, line_width=50, tension=1)
+
+scene.add(trace)
+# trace.t.add_animation(Animation(0, 24, 0, 1, easing.CubicEaseInOut))
+
+for i, obj in enumerate(trace.objects):
+    obj.animate("y", PingPong(Animation(0, 6, -10*(i), 10*(i), easing.CircularEaseInOut, AnimationType.ADDITIVE), 10))
+
+scene.preview()
