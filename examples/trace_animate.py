636da9ef9d46a53eab2c250f435cdc4feb219736
Doug Mercer <dougmerceryt@gmail.com>
2024-05-10 18:33:20 -0400

    lint

--- examples/trace_animate.py
+++ examples/trace_animate.py
@@ -5,7 +5,7 @@ with open("examples/example.py", "r") as f:
 styled_tokens = tokenize(content)
 
 scene = Scene(scene_name="trace", num_frames=24, width=1920, height=1080)
-code = Code(scene, styled_tokens, font_size=48, alpha=1, x=100, y=100)
+code = Code(scene, styled_tokens, font_size=48, x=100, y=100)
 
 scene.add(code)
 
@@ -13,9 +13,8 @@ trace = Trace(scene, code.lines[:4].chars.filter_whitespace(), alpha=0.5, line_w
 
 scene.add(trace)
 
-# trace.t.add_animation(Animation(0, 24, 0, 1, easing.CubicEaseInOut))
 trace.shift(0, 100, 0, 10)
 
-code.lines[0:4].shift(100, 0, 0, 10)
+code.lines[:4].shift(100, 0, 0, 10)
 
 scene.preview()
