6e3de1e345a030a7236876ed5b0c6482fc242957
Doug Mercer <dougmerceryt@gmail.com>
2024-06-09 09:52:34 -0400

    Rotations are no longer additive.

--- examples/rotate_text.py
+++ examples/rotate_text.py
@@ -1,18 +1,14 @@
-from keyed import Animation, AnimationType, Code, Scene, tokenize
+from keyed import Animation, Code, Scene, tokenize
 
 with open("examples/example.py", "r") as f:
     content = f.read()
 styled_tokens = tokenize(content)
 
-scene = Scene(scene_name="write_on_tokens", num_frames=24, width=1920, height=1080)
+scene = Scene(num_frames=24, width=1920, height=1080)
 code = Code(scene, styled_tokens, font_size=48, alpha=1)
 scene.add(code)
 
-# These should basically cancel out, slightly rotating counter clockwise
-code.lines[3:].rotate(Animation(0, 12, 0, -180, animation_type=AnimationType.ADDITIVE))
-code.lines[3:].rotate(Animation(0, 12, 0, 170, animation_type=AnimationType.ADDITIVE))
-
-# This one rotates normally
-code.lines[0].rotate(Animation(0, 12, 0, 180, animation_type=AnimationType.ABSOLUTE))
+code.lines[3:].rotate(Animation(0, 12, 0, -10))
+code.lines[0].rotate(Animation(0, 12, 0, 180))
 
 scene.preview()
